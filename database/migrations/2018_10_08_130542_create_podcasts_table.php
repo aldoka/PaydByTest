42506ccf4cb30cf3a29fcfb35e2a47ec8caa2a29
Sergey Kuzminich <KuzminichSergey@yandex.ru>
2018-10-08 13:32:45 +0300

    Podcasts migration created

---
+++ database/migrations/2018_10_08_130542_create_podcasts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePodcastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('podcasts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->string('name', 256)->unique();
+            $table->string('description', 1000);
+            $table->string('url', 256);
+            $table->string('image', 256)->nullable();
+            $table->unsignedTinyInteger('status')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('podcasts');
+    }
+}
+
