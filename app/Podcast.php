bd5fb0a2636c142032c9f86e403d4d1ad5a8cb5b
Sergey Kuzminich <KuzminichSergey@yandex.ru>
2018-10-09 00:36:49 +0300

    Doc block improved

--- app/Podcast.php
+++ app/Podcast.php
@@ -15,29 +15,27 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property string|null $deleted_at
  * @property string $name
  * @property string $description
- * @property string $url
+ * @property string|null $marketing_url
+ * @property string $feed_url
  * @property string|null $image
  * @property int $status
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Comment[] $comments
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Podcast whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Podcast whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Podcast whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Podcast whereFeedUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Podcast whereMarketingUrl($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Podcast whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Podcast whereImage($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Podcast whereName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Podcast whereStatus($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Podcast whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Podcast whereUrl($value)
- * @mixin \Eloquent
- * @property string|null $marketing_url
- * @property string $feed_url
  * @method static bool|null forceDelete()
  * @method static \Illuminate\Database\Query\Builder|\App\Podcast onlyTrashed()
  * @method static bool|null restore()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Podcast whereFeedUrl($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Podcast whereMarketingUrl($value)
  * @method static \Illuminate\Database\Query\Builder|\App\Podcast withTrashed()
  * @method static \Illuminate\Database\Query\Builder|\App\Podcast withoutTrashed()
+ * @mixin \Eloquent
  */
 class Podcast extends Model
 {
