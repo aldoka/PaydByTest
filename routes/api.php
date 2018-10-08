67810207ce72d19dd05ac6789a7a2ee585bb1842
Sergey Kuzminich <KuzminichSergey@yandex.ru>
2018-10-08 20:12:04 +0300

    Default throttling added

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Http\Request;
 //});
 
 $api = app('Dingo\Api\Routing\Router');
-$api->version('v1', function ($api) {
-    $api->get('podcasts/{id}', 'App\Http\Controllers\PodcastController@show');
-    $api->get('podcasts', 'App\Http\Controllers\PodcastController@index');
+$api->version('v1', ['middleware' => 'api.throttle'], function ($api) {
+    $api->get('podcasts/{status}', 'App\Http\Controllers\PodcastController@index')->where('status', '[A-Za-z]+');
+    $api->get('podcasts/{id}', 'App\Http\Controllers\PodcastController@show')->where('id', '[0-9]+');
 });
