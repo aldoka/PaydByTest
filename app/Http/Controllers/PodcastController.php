be108042e83a28a4328f0a25d564a8ec3a7fa559
Sergey Kuzminich <KuzminichSergey@yandex.ru>
2018-10-08 20:32:57 +0300

    Exception handling added to Podcast controller

--- app/Http/Controllers/PodcastController.php
+++ app/Http/Controllers/PodcastController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Transformers\PodcastTransformer;
 use App\Podcast;
 use App\Scopes\PodcastScope;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 /**
@@ -22,6 +23,12 @@ class PodcastController extends BaseController
     /** @var string  */
     const QUERY_STATUS_REVIEW = 'review';
 
+
+    const QUERY_STATUSES = [
+        self::QUERY_STATUS_PUBLISHED,
+        self::QUERY_STATUS_REVIEW,
+    ];
+
     /**
      * Display a list of Podcasts.
      *
@@ -30,12 +37,23 @@ class PodcastController extends BaseController
      */
     public function index(string $status)
     {
-        if ($status === self::QUERY_STATUS_REVIEW) {
-            $podcasts = Podcast::withoutGlobalScope(PodcastScope::class)
-                ->where('status', Podcast::STATUS_REVIEW)
-                ->paginate(self::ITEMS_PER_PAGE);
-        } else {
-            $podcasts = Podcast::paginate(self::ITEMS_PER_PAGE);
+        if (in_array($status, self::QUERY_STATUSES) === false) {
+            throw new Dingo\Api\Exception\ResourceException('Unknown status filter');
+        }
+
+        try {
+            switch($status) {
+                case self::QUERY_STATUS_PUBLISHED:
+                    $podcasts = Podcast::paginate(self::ITEMS_PER_PAGE);
+                    break;
+                case self::QUERY_STATUS_REVIEW:
+                    $podcasts = Podcast::withoutGlobalScope(PodcastScope::class)
+                        ->where('status', Podcast::STATUS_REVIEW)
+                        ->paginate(self::ITEMS_PER_PAGE);
+                    break;
+            }
+        } catch (\Exception $e) {
+            throw new \Dingo\Api\Exception\ResourceException('Application was unable process this podcasts list');
         }
 
         return $this->response->paginator($podcasts, new PodcastTransformer);
@@ -61,7 +79,16 @@ class PodcastController extends BaseController
      */
     public function show(Podcast $podcast, int $id)
     {
-        return $this->response->item($podcast->findOrFail($id), new PodcastTransformer);
+        $result = null;
+        try{
+            $result = $podcast->findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            throw new \Dingo\Api\Exception\ResourceException('Application can\'t find podcast with such id');
+        } catch (\Exception $e) {
+            throw new \Dingo\Api\Exception\ResourceException('Application was unable process this podcast');
+        }
+
+        return $this->response->item($result, new PodcastTransformer);
     }
 
     /**
