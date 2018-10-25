203630c714511124a8afe1e449208b12230a35d0
Sergey Kuzminich <KuzminichSergey@yandex.ru>
2018-10-25 17:05:45 +0300

    Podcast faker improved to generate unique data

--- database/factories/PodcastFactory.php
+++ database/factories/PodcastFactory.php
@@ -7,12 +7,16 @@ use Faker\Generator as Faker;
 $factory->define(
     Podcast::class,
     function (Faker $faker) {
-        $company = $faker->company;
+        $company      = $faker->company;
+        $name         = mb_substr($company, 0, 123) . mt_rand(0, 10000);
+        $marketingUrl = $faker->url . mt_rand(0, 10000);
+        $feedUrl      = $faker->url . mt_rand(0, 10000);
+
         return [
-            'name' => mb_substr($company, 0, 128),
+            'name' => $name,
             'description'   => $faker->realText(1000),
-            'marketing_url' => $faker->url,
-            'feed_url' => $faker->url,
+            'marketing_url' => $marketingUrl,
+            'feed_url' => $feedUrl,
             'image'    => $faker->imageUrl(),
             'status'   => $faker->randomElement(Podcast::getAllStatuses()),
         ];
